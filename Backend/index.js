646f3bab691f0b3da96e6b208d240546d077e501
p6527 <harshchaudry2004@gmail.com>
2026-01-02 09:35:01 +0530

    change

--- Backend/index.js
+++ Backend/index.js
@@ -10,12 +10,9 @@ dotenv.config();
 
 const app = express();
 
-// CORS configuration with correct frontend URL
 app.use(cors({
-  origin: ['https://bookapp-1-n1lr.onrender.com', 'http://localhost:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*',
+  credentials: false
 }));
 
 // Parse JSON bodies
